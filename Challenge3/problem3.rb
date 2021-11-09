363882314bcf444e63f96f5ba7d48d2f6011e252
Marco Evair <mevairmc@gmail.com>
2021-11-08 20:11:27 -0500

    Challenge 3: plus_one

---
+++ Challenge3/problem3.rb
@@ -0,0 +1,5 @@
+class Problem3
+    def self.plus_one(s)
+        #write your code here
+    end
+end
---
+++ Challenge3/problem3_test.rb
@@ -0,0 +1,13 @@
+require "minitest/autorun"
+require_relative "./problem2"
+
+class Problem3Test < Minitest::Test
+    def test_is_valid_bracket
+        assert_equal [1,2,4], Problem3.plus_one("digits = [1,2,3]")
+        assert_equal [4,3,2,2], Problem3.plus_one("digits = [4,3,2,1]")
+        assert_equal [1], Problem3.plus_one("digits = [0]")
+        assert_equal [1,0], Problem3.plus_one("digits = [9]")
+        assert_equal [1,2,3,4,5,9,7,5,8,4,6,7,8,4,5,3,4,5,1,2,1], Problem3.plus_one("digits = [1,2,3,4,5,9,7,5,8,4,6,7,8,4,5,3,4,5,1,2,0]")
+        assert_equal [1,2,4,0], Problem3.plus_one("digits = [1,2,3,9]")
+    end
+end
